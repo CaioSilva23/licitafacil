5d18529b002c54e99c6a415f5094227b0ba9b61b
caio silva <caio_ceac23@hotmail.com>
2023-01-05 15:27:24 -0300

    setting

--- licitafacil/settings.py
+++ licitafacil/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=False)
 
-#ALLOWED_HOSTS = ['licitafacil.vps-kinghost.net', '189.126.111.167', 'licitafacilbr.com.br']
+ALLOWED_HOSTS = ['licitafacil.vps-kinghost.net', '189.126.111.167', 'licitafacilbr.com.br']
 
 
 # Application definition
