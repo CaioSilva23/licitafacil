d5554d04a0bf0d643e872f2abb18ddba8403eb64
caio silva <caio_ceac23@hotmail.com>
2023-03-07 21:00:43 -0300

    test

--- licitafacil/celery.py
+++ licitafacil/celery.py
@@ -1,14 +1,15 @@
 import os
 from celery import Celery
+from django.conf import settings
 # celery -A projeto_celery worker --loglevel=INFO
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'licitafacil.settings')
 
 app = Celery('licitafacil')
 
-app.config_from_object('django.conf.settings', namespace='CELERY')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 @app.task(bind=True)
 def debug_task(self):
